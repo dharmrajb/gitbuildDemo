c34111d100a67ff465950375efa76df6d05096c7
Lenovo <dharmrajmca11@gmail.com>
2023-01-08 23:44:19 +0530

    Commit code by Dhrmraj

---
+++ gitbuildDemo/src/main/java/com/gitbuildDemo/App.java
@@ -0,0 +1,13 @@
+package com.gitbuildDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
